61757593a036460f4d6c1f891934bf8184b64518
AlmiraNechaeva <anechaeva1@outlook.com>
2023-04-25 09:47:49 +0300

    Added Task 25, 27, 29б

---
+++ Task27/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// используем while тк неизвестно кол-во итерраций
+
+// Console.WriteLine("Введите целое число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int sumNumbers = SumNumbers(number);
+// Console.WriteLine ($"Сумма цифр в числе {number} = {sumNumbers}");
+// int SumNumbers(int num)
+// {
+//     int sum = 0;
+//     while (sum > 0; i <= num; i++) 
+//     {
+//         checked
+//         {
+//         sum *= i;    // иначе -> sum = sum + 1 // sum/=2; // 
+//         }          
+//     }  
+    
+//     return sum;   // сели знаем колво итенраций то for?елси не знаем while
+
+// }
+
+
+int number = ReadInt("Введите число: ");
+int length = NumberLength(number);
+SumNumbers(number, length);
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int NumberLength(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
---
+++ Task29/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+//как 30 - диапозон от 0 до 100 - 1 массив
+//заполненный
+// нулями и единицами в случайном порядке
+
+Console.WriteLine("Введите длину массива");
+int lenghtArray = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArray(lenghtArray);
+PrintArray(arr);
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];    /// заполнится нулями без скобо {} или //int[] arr = {4,6,6,}
+    Random random = new Random(); //оптимизация объекта Random (var - объект неявной типизации)
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 100);   //01234567
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ($" {array[i]}"); //($"Вторая цифра, '{secondRank}'");
+    }
+}
+// Console.ReadKey();
+//}
